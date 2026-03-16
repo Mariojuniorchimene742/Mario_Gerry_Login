3873ef532feae14616e84ffa49090fe5c0a22c73
Mariojuniorchimene <36141@esjaloures.org>
2026-03-16 14:47:59 +0000

    alteraçoes

---
+++ app/src/main/java/com/example/loginappmario_gerry/SignupActivity2.java
@@ -0,0 +1,14 @@
+package com.example.loginappmario_gerry;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup3);
+    }
+}
